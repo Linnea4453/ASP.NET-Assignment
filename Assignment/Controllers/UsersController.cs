0dda917be28d83be8760260977946fc54a74923d
Linnea Nilsson <linnea.nilsson23@learnet.se>
2021-02-16 13:37:27 +0100

    init

---
+++ Assignment/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment.Services.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly IIdentityService _identityService;
+
+        public UsersController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Users = await _identityService.GetAllUsersWithRolesAsync();
+            ViewBag.Roles = _identityService.GetAllRoles();
+         
+
+            return View();
+        }
+    }
+}
